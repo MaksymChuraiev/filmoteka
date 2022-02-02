38543c6f9f6ef9c4322582d0baf0706543af8bfd
MaksymChuraiev <mchuraev1988@gmail.com>
2022-02-03 00:43:52 +0200

    add 02.02 night

--- src/js/components/switcher.js
+++ src/js/components/switcher.js
@@ -3,6 +3,7 @@ console.log(checkboxBtn);
 const body = document.querySelector('body');
 const modal = document.querySelector('.modal-container');
 const footer = document.querySelector('.footer');
+const modalFilm = document.querySelector('.modal-film-card');
 
 const Theme = {
   LIGHT: 'light-theme',
@@ -17,20 +18,24 @@ function changeTheme(e) {
   const theme = e.target.checked;
   if (theme) {
     body.classList.add(DARK);
+    modalFilm.classList.add(DARK);
     modal.classList.add(DARK);
     footer.classList.add(DARK);
 
     body.classList.remove(LIGHT);
+    modalFilm.classList.remove(LIGHT);
     modal.classList.remove(LIGHT);
     footer.classList.remove(LIGHT);
 
     localStorage.setItem('theme', DARK);
   } else {
     body.classList.add(LIGHT);
+    modalFilm.classList.add(LIGHT);
     modal.classList.add(LIGHT);
     footer.classList.add(LIGHT);
 
     body.classList.remove(DARK);
+    modalFilm.classList.remove(DARK);
     modal.classList.remove(DARK);
     footer.classList.remove(DARK);
 
@@ -45,6 +50,7 @@ if (!theme) {
   localStorage.setItem('theme', theme);
 }
 body.classList.add(theme);
+modalFilm.classList.add(theme);
 modal.classList.add(theme);
 footer.classList.add(theme);
 
